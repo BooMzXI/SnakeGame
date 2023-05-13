9d251c8e0c59126e5fba310d6f1eaa197003554d
Patipoll <ezboomgg123@gmail.com>
2023-05-13 14:37:03 +0700

    add bonus && max score && Fixed problem

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let canvas, context
 let scores = 0;
+const button = document.querySelectorAll('button');
 
 window.onload = () => {
     canvas = document.getElementById('canvas');
@@ -7,7 +8,47 @@ window.onload = () => {
 
     document.addEventListener('keydown', keyDownEvent);
 
-
+    button.forEach(btn => {
+        btn.addEventListener('click', e => {
+            button.forEach(btn => {
+                btn.addEventListener('click', e => {
+                    const btn = e.target;
+                    const id = btn.getAttribute('id')
+                    switch (id) {
+                        case '65': //a
+                            if (nextX !== 1) { // prevent reversing direction
+                                nextX = -1;
+                                nextY = 0;
+                            }
+                            break;
+                        case '87': //w
+                            if (nextY !== 1) { // prevent reversing direction
+                                nextX = 0;
+                                nextY = -1;
+                                up = 1;
+                                down = 0;
+                            }
+                            break;
+                        case '68': //d
+                            if (nextX !== -1 ) { // prevent reversing direction
+                                nextX = 1;
+                                nextY = 0;
+                            }
+                            break;
+                        case '83': 
+                            if (nextY !== -1 ) { // prevent reversing direction
+                                nextX = 0;
+                                nextY = 1;
+                                up = 0;
+                                down = 1;
+                            }
+                            break;
+                    }
+                })
+            })
+            
+        })
+    })
     //render speed times pre sec
     let speed = 8;
     setInterval(draw, 1000 / speed);
@@ -21,7 +62,9 @@ window.onload = () => {
     let snakeTail = [];
     let positionX = positionY = 10;
     let appleOnSnake = false;
+    let max = 0;
 
+    let yellowX = yellowY = 18; 
     let appleX = appleY = 15;
 
     function draw() {
@@ -79,7 +122,7 @@ window.onload = () => {
             }
             
         }
-
+        //canvas
         context.fillStyle = "gray";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -94,6 +137,10 @@ window.onload = () => {
             )
             //Snake bite its tail
                 if (snakeTail[i].x == positionX && snakeTail[i].y == positionY){
+                    if (scores > max){
+                        max = scores;
+                        document.getElementById('Max').innerHTML = max;
+                    }
                     tailSize = defTailSize;
                     scores = 0;
                     document.getElementById('score').innerHTML = scores;
@@ -104,6 +151,18 @@ window.onload = () => {
         context.fillStyle = 'red';
         context.fillRect(appleX * tileSize, appleY * tileSize , tileSize , tileSize)
 
+        //Yellow
+        if (scores % 50 == 0 && scores != 0) {
+            context.fillStyle = 'yellow';
+            context.fillRect(yellowX * tileSize, yellowY * tileSize , tileSize , tileSize)
+            if (positionX == yellowX && positionY == yellowY){
+                tailSize += 2;
+                scores += 20;
+                document.getElementById('score').innerHTML = scores;
+                yellowX = Math.floor(Math.random() * gridSize)
+                yellowY = Math.floor(Math.random() * gridSize);
+            }
+        }
 
         //Set snake tail
         snakeTail.push({ x: positionX, y: positionY});
@@ -114,21 +173,33 @@ window.onload = () => {
 
     function keyDownEvent(e) {
         switch (e.keyCode) {
-            case 65: 
-                nextX = -1;
-                nextY = 0;
+            case 65: //a
+                if (nextX !== 1) { // prevent reversing direction
+                    nextX = -1;
+                    nextY = 0;
+                }
                 break;
-            case 87: 
-                nextX = 0;
-                nextY = -1;
+            case 87: //w
+                if (nextY !== 1) { // prevent reversing direction
+                    nextX = 0;
+                    nextY = -1;
+                    up = 1;
+                    down = 0;
+                }
                 break;
-            case 68: 
-                nextX = 1;
-                nextY = 0;
+            case 68: //d
+                if (nextX !== -1) { // prevent reversing direction
+                    nextX = 1;
+                    nextY = 0;
+                }
                 break;
             case 83: 
-                nextX = 0;
-                nextY = 1;
+                if (nextY !== -1) { // prevent reversing direction
+                    nextX = 0;
+                    nextY = 1;
+                    up = 0;
+                    down = 1;
+                }
                 break;
         }
     }
